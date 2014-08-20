147960665b016ff210970cace42fde50e6c0deee
asukaceres <asukaceres@gmail.com>
2014-08-20 14:21:19 +0800

    Merge svn 423 (Release 0.6.10)

--- main/logbook/gui/logic/CreateReportLogic.java
+++ main/logbook/gui/logic/CreateReportLogic.java
@@ -210,8 +210,9 @@ public final class CreateReportLogic {
             Arrays.fill(enemyHp, "");
 
             if (battle != null) {
-                DockDto dock = battle.getDock();
-                if (dock != null) {
+                List<DockDto> docks = battle.getFriends();
+                if (docks != null) {
+                    DockDto dock = docks.get(0);
                     List<ShipDto> friendships = dock.getShips();
                     int[] fnowhps = battle.getNowFriendHp();
                     int[] fmaxhps = battle.getMaxFriendHp();
@@ -310,7 +311,7 @@ public final class CreateReportLogic {
      * @return ヘッダー
      */
     public static String[] getItemListHeader() {
-        return new String[] { "", "名称", "種別", "個数", "火力", "命中", "射程", "運", "回避", "爆装", "雷装", "索敵", "対潜", "対空" };
+        return new String[] { "", "名称", "種別", "個数", "火力", "命中", "射程", "運", "回避", "爆装", "雷装", "索敵", "対潜", "対空", "装甲" };
     }
 
     /**
@@ -349,7 +350,7 @@ public final class CreateReportLogic {
             count++;
             body.add(new Object[] { count, item.getName(), item.getType(), entry.getValue(), item.getHoug(),
                     item.getHoum(), item.getLeng(), item.getLuck(), item.getHouk(), item.getBaku(), item.getRaig(),
-                    item.getSaku(), item.getTais(), item.getTyku()
+                    item.getSaku(), item.getTais(), item.getTyku(), item.getSouk()
             });
         }
         return toListStringArray(body);
@@ -422,6 +423,8 @@ public final class CreateReportLogic {
                 long raisou = ship.getRaisouMax() - ship.getRaisou();
                 // 対空
                 long taiku = ship.getTaikuMax() - ship.getTaiku();
+                // 装甲
+                long soukou = ship.getSoukouMax() - ship.getSoukou();
                 // 回避
                 long kaihi = ship.getKaihiMax() - ship.getKaihi();
                 // 対潜
@@ -436,6 +439,7 @@ public final class CreateReportLogic {
                         karyoku += item.getHoug();
                         raisou += item.getRaig();
                         taiku += item.getTyku();
+                        soukou += item.getSouk();
                         taisen += item.getTais();
                         sakuteki += item.getSaku();
                         lucky += item.getLuck();
@@ -462,7 +466,7 @@ public final class CreateReportLogic {
                         karyoku,
                         raisou,
                         taiku,
-                        ship.getSoukouMax() - ship.getSoukou(),
+                        soukou,
                         kaihi,
                         taisen,
                         sakuteki,
